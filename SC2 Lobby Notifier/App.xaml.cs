4213aaf2ebe74a8ec14c7470a555613bb037bbc6
B4dName <109242827+B4dName@users.noreply.github.com>
2022-10-28 15:49:43 +0300

    Code that work on start of program

---
+++ SC2 Lobby Notifier/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using System.Windows;
+
+namespace SC2_Lobby_Notifier
+{
+    public partial class App : Application
+    {
+        // Предотвращение запуска более 1 приложения
+        Mutex myMutex;
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            bool isNewInstance = false;
+            myMutex = new Mutex(true, "SC2_Lobby_Notifier", out isNewInstance);
+            if (!isNewInstance)
+            {
+                Current.Shutdown();
+            }
+        }
+    }
+}
